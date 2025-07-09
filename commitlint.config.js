f9eaa1fc47bdf86f138ecfb56e3fa38db6774963
m-henrique <39414748+matixinha@users.noreply.github.com>
2025-07-09 17:35:49 -0300

    chore(repo): adicionar Commitlint e Husky para validar mensagens de commit

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default { extends: ['@commitlint/config-conventional'] ,
+    rules: {
+        'header-max-length': [2, 'always', 100],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+                'build',
+                'ci'
+            ]
+        ],
+        'scope-empty': [2, 'never'],
+        'scope-enum': [2, 'always', ['ui', 'core', 'repo']]
+    }
+};
